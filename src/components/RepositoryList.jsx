05c8e3630baaea7fde417626957f0e992d35c79f
kahmariano <kmariano138@gmail.com>
2021-11-05 20:58:46 -0300

    primeiro componente de react

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,38 @@
+export function RepositoryList(){
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong> unform </strong>
+          <p>Forms in React</p>
+          <a href="">
+            Aessar repositorio
+          </a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <strong> unform </strong>
+          <p>Forms in React</p>
+          <a href="">
+            Aessar repositorio
+          </a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <strong> unform </strong>
+          <p>Forms in React</p>
+          <a href="">
+            Aessar repositorio
+          </a>
+        </li>
+      </ul>
+
+    </section>
+  )
+}
